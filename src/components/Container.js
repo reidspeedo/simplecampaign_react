b008b2c4d00528ad88a949e295b170516aa55090
Reid Relatores <rrrelatores@gmail.com>
2021-06-29 14:34:23 -0700

    updated container header

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,33 +1,50 @@
-import Button from "./Button"
+import MyButton from "./MyButton"
+
 import SearchBar from 'material-ui-search-bar';
+import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear'
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
     root: {
         boxSizing: 'border-box',
         boxShadow: 'none',
-        fontSize: '1px',
         borderColor: '#7976FF',
         border: 'solid',
         borderWidth: '1pt',
         marginRight: '5%',
         height: '25pt',
+    },
+    icons: {
+        fontSize: 20,
+        color: '#7976FF',
+        paddingLeft: 0,
+    },
+    inputs: {
+        marginTop: '1%',
+        fontSize: 14,
+        fontFamily: '"Lucida Console", "Courier New", monospace'
     }
 })
+
+// {marginTop: '1%', fontSize: 14}
 
 const Container = () => {
     const classes = useStyles();    
     return (
         <>
             <div className='container-header'>
-                <div>
-                    <Button text={'Add'}/>
-                    <Button text={'Edit All'}/>
-                    <Button text={'Update'}/>
-                    <Button text={'Lookup'}/>
+                <div className = 'container-header-left'>
+                    <MyButton text={'Add'}/>
+                    <MyButton text={'Edit All'}/>
+                    <MyButton text={'Update'}/>
+                    <MyButton text={'Lookup'}/>
                 </div>
                 <div>
-                    <SearchBar style={{fontSize: '1pt'}}placeholder='' className={classes.root}/>
+                    <SearchBar
+                    closeIcon={<ClearIcon className={classes.icons}/>} 
+                    searchIcon={<SearchIcon className={classes.icons}/>} 
+                    inputProps={{className: classes.inputs}} placeholder='' className={classes.root}/>
                 </div>
             </div>
             <div className='container-main'>container-main</div>
---
+++ src/components/MyButton.js
@@ -0,0 +1,31 @@
+import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core";
+
+
+const MyButton = ({text}) => {
+    const useStyles = makeStyles({
+        root: {
+            fontFamily: '"Lucida Console", "Courier New", monospace',
+            fontWeight: 'bold',
+            color: '#7976FF',
+            borderColor: '#7976FF',
+            '&:hover': {
+                color: 'white',
+                backgroundColor: '#7976FF',
+             },
+        }
+    })
+
+    const classes = useStyles()
+    // style={{borderColor: color, color: color}}
+    return (
+        <Button variant='outlined' className={classes.root}>{text}</Button>
+        // <button style={{backgroundColor: color}} className='btn'>{text}</button>     
+    )
+}
+
+MyButton.defaultProps = {
+    text: 'Click',
+}
+
+export default MyButton
